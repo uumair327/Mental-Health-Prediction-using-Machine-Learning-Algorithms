ec96a63242a203dded9d8062c71b49b0675690ac
uumair327 <mohdumair.a@somaiya.edu>
2024-10-06 17:10:23 +0530

    Changes

--- Web page/mental_health.py
+++ Web page/mental_health.py
@@ -3,49 +3,69 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.naive_bayes import GaussianNB 
+from sklearn.naive_bayes import GaussianNB
 from mlxtend.classifier import StackingClassifier
 from sklearn.model_selection import train_test_split
 import warnings
 import pickle
+
 warnings.filterwarnings("ignore")
 
-data = pd.read_csv("mental_health.csv")
-male_str = ["male", "m", "male-ish", "maile", "mal", "male (cis)", "make", "male ", "man","msle", "mail", "malr","cis man", "Cis Male", "cis male"]
-trans_str = ["trans-female", "something kinda male?", "queer/she/they", "non-binary","nah", "all", "enby", "fluid", "genderqueer", "androgyne", "agender", "male leaning androgynous", "guy (-ish) ^_^", "trans woman", "neuter", "female (trans)", "queer", "ostensibly male, unsure what that really means"]           
-female_str = ["cis female", "f", "female", "woman",  "femake", "female ","cis-female/femme", "female (cis)", "femail"]
+# Load the dataset
+data = pd.read_csv("C:/Users/uumai/OneDrive/Desktop/Mental-Health-Prediction-using-Machine-Learning-Algorithms/Web page/mental_health.csv")
 
+
+# Define lists for gender mapping
+male_str = ["male", "m", "male-ish", "maile", "mal", "male (cis)", "make", "male ", "man", "msle", "mail", "malr", "cis man", "Cis Male", "cis male"]
+trans_str = ["trans-female", "something kinda male?", "queer/she/they", "non-binary", "nah", "all", "enby", "fluid", "genderqueer", "androgyne", "agender", 
+             "male leaning androgynous", "guy (-ish) ^_^", "trans woman", "neuter", "female (trans)", "queer", "ostensibly male, unsure what that really means"]
+female_str = ["cis female", "f", "female", "woman", "femake", "female ", "cis-female/femme", "female (cis)", "femail"]
+
+# Standardize the Gender column
 for (row, col) in data.iterrows():
-
-    if str.lower(col.Gender) in male_str:
+    gender_lower = str.lower(col.Gender)
+    if gender_lower in male_str:
         data['Gender'].replace(to_replace=col.Gender, value='male', inplace=True)
-
-    if str.lower(col.Gender) in female_str:
+    elif gender_lower in female_str:
         data['Gender'].replace(to_replace=col.Gender, value='female', inplace=True)
-
-    if str.lower(col.Gender) in trans_str:
+    elif gender_lower in trans_str:
         data['Gender'].replace(to_replace=col.Gender, value='trans', inplace=True)
 
-#Get rid of bullshit
+# Get rid of unwanted values
 stk_list = ['A little about you', 'p']
-data = data[~train_df['Gender'].isin(stk_list)]
-data['Gender']=data['Gender'].map({'male':0,'female':1, 'trans':2})
-data['family_history']=data['family_history'].map({'No':0,'Yes':1})
-data['treatment']=data['treatment'].map({'No':0,'Yes':1})
+data = data[~data['Gender'].isin(stk_list)]
 
+# Map categorical values to numeric
+data['Gender'] = data['Gender'].map({'male': 0, 'female': 1, 'trans': 2})
+data['family_history'] = data['family_history'].map({'No': 0, 'Yes': 1})
+data['treatment'] = data['treatment'].map({'No': 0, 'Yes': 1})
+
+# Convert to numpy array
 data = np.array(data)
 
-X = data[1:,1:-1]
+# Prepare the features (X) and target (y) variables
+X = data[1:, 1:-1]
 y = data[1:, -1]
 y = y.astype('int')
 X = X.astype('int')
+
+# Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+# Initialize classifiers
 clf1 = KNeighborsClassifier(n_neighbors=1)
 clf2 = RandomForestClassifier(random_state=1)
 clf3 = GaussianNB()
 lr = LogisticRegression()
+
+# Stack classifiers using a meta-classifier (Logistic Regression)
 stack = StackingClassifier(classifiers=[clf1, clf2, clf3], meta_classifier=lr)
+
+# Train the stacked model
 stack.fit(X_train, y_train)
 
-pickle.dump(stack,open('model.pkl','wb'))
-model=pickle.load(open('model.pkl','rb'))
+# Save the trained model to a file
+pickle.dump(stack, open('Web page/model.pkl', 'wb'))
+
+# Load the model from the file for future use
+model = pickle.load(open('Web page/model.pkl', 'rb'))
